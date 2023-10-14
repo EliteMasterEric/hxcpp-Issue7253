dae6397c3e848776cb695080c2d2d7998a9e10ce
EliteMasterEric <ericmyllyoja@gmail.com>
2023-10-13 20:02:21 -0400

    Cleanup.

--- src/Main.hx
+++ src/Main.hx
@@ -1,47 +1,42 @@
 package;
 
-interface ISomeInterface
-{
+interface ISomeInterface {
 	function getSomeProperty():Float;
 }
 
 class SomeClass {
-  var nullPointer:ISomeInterface;
+	var nullPointer:ISomeInterface;
 
-  public var someProperty(get, null):Float;
+	public var someProperty(get, null):Float;
 
-  public function new() {}
+	public function new() {}
 
-  function get_someProperty():Float {
-    return nullPointer.getSomeProperty();
-  }
+	function get_someProperty():Float {
+		return nullPointer.getSomeProperty();
+	}
 }
 
-class Main
-{
- public static function main(): Void
- {
-      var inst = new SomeClass();
-      trace("Begin");
-      trace("Begin case 1");
-      try {
-        var unexistProperty = Reflect.getProperty(inst, "unexistProperty");
-        trace(unexistProperty.some); 
-      }
-      catch(e:Dynamic) {
-        trace(e); //Null Object Reference - works as expected
-      }
-      trace("End case 1");
+class Main {
+	public static function main():Void {
+		var inst = new SomeClass();
+		trace("Begin");
+		trace("Begin case 1");
+		try {
+			var unexistProperty = Reflect.getProperty(inst, "unexistProperty");
+			trace(unexistProperty.some);
+		} catch (e:Dynamic) {
+			trace(e); // Null Object Reference - works as expected
+		}
+		trace("End case 1");
 
-      trace("Begin case 2");
-      try {
-        var val = Reflect.getProperty(inst, "someProperty"); //Just crash
-        trace(val);
-      }
-      catch(e:Any) {
-        trace(e);
-      }
-      trace("End case 2");
-      trace("Finish");
- }
+		trace("Begin case 2");
+		try {
+			var val = Reflect.getProperty(inst, "someProperty"); // Just crash
+			trace(val);
+		} catch (e:Any) {
+			trace(e);
+		}
+		trace("End case 2");
+		trace("Finish");
+	}
 }
